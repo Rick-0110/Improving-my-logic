7efa7ae78a0aeae1daf6b0c12e786d5082dd077f
Henrique <matosh1604@gmail.com>
2025-09-07 11:48:14 -0300

    GradeAverage(Média alunos)

---
+++ Lógica/GradeAverage/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+class GradeAverage
+{
+    static void main(string[] args)
+    {
+        Console.WriteLine("Digite a sua nota na primeira unidade:");
+        double nota1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a sua nota na segunda unidade:");
+        double nota2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a sua nota na terceira unidade:");
+        double nota3 = double.Parse(Console.ReadLine());
+
+        double media = (nota1 + nota2 + nota3) / 3;
+
+
+        if (media >= 7)
+        {
+            Console.WriteLine($"Você foi aprovado com média {media}");
+        }
+        else
+        {
+            Console.WriteLine($"Você foi reprovado com média {media}");
+        }
+
+    }
+}
+
+
