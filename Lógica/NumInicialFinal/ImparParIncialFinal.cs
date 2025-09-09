58440eb667ed3303db98b1c59cb61c8f52e8409f
Henrique <matosh1604@gmail.com>
2025-09-09 14:10:19 -0300

    Escolher um numero inicial e um final e ver quais são impar e quais são par

---
+++ Lógica/NumInicialFinal/ImparParIncialFinal.cs
@@ -0,0 +1,24 @@
+﻿class ImparParIncialFinal
+{
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Digite um número inicial");
+        int numInicial = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite um número final");
+        int numFinal = int.Parse(Console.ReadLine());
+
+        for (int i = numFinal; i >= numInicial; i--)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine($"O número {i} é par");
+            }
+            else
+            {
+                Console.WriteLine($"O número {i} é impar");
+            }
+        }
+    }
+}
--- Lógica/Program.cs
+++ Lógica/Program.cs
@@ -1,13 +1,1 @@
-﻿//Aqui coloco os códigos para rodar
-
-Console.WriteLine("Digite um número para ver-mos se é impar ou par");
-int num = int.Parse(Console.ReadLine());
-
-if(num % 2 == 0)
-{
-    Console.WriteLine("Numero par");
-}
-else
-{
-       Console.WriteLine("Numero impar");
-}
+﻿
