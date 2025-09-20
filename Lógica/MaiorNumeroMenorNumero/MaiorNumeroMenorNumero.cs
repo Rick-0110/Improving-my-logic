b34d0db4cf77924139145af42861e73ac77e5f16
Henrique <matosh1604@gmail.com>
2025-09-20 14:38:20 -0300

    Fiz uma lógica onde o usuário escolhe a quatidade de números que quer digitar e depois aparece o maior e o menor número

---
+++ Lógica/MaiorNumeroMenorNumero/MaiorNumeroMenorNumero.cs
@@ -0,0 +1,45 @@
+﻿
+class MaiorNumeroMenorNumero
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Quantos números você deseja digitar?: ");
+        int N = int.Parse(Console.ReadLine());
+
+        int[] numeros = new int[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            Console.WriteLine($"Digite o {i + 1}º número: ");
+            numeros[i] = int.Parse(Console.ReadLine());
+
+            if (numeros[i] < 0)
+            {
+                Console.WriteLine("Número inválido! Digite um número não negativo.");
+                i--;
+            }
+        }
+
+        int maior = numeros[0];
+        for (int i = 1; i < N; i++)
+        {
+            if (numeros[i] > maior)
+            {
+                maior = numeros[i];
+            }
+        }
+
+        Console.WriteLine($"O maior número digitado foi: {maior} ");
+
+        int menor = numeros[0];
+
+        for (int i = 1; i < N; i++)
+        {
+            if (numeros[i] < menor)
+            {
+                menor = numeros[i];
+            }
+        }
+        Console.WriteLine($"O menor número digitado foi: {menor}");
+    }
+}
--- Lógica/QuantidadeImparesPares/QuantidadeImparesPares.cs
+++ Lógica/QuantidadeImparesPares/QuantidadeImparesPares.cs
@@ -4,34 +4,35 @@
     {
 
         Console.WriteLine("Say the first number");
-int FirstNum = int.Parse(Console.ReadLine());
+        int FirstNum = int.Parse(Console.ReadLine());
 
-Console.WriteLine("say the last number");
-int LastNum = int.Parse(Console.ReadLine());
+        Console.WriteLine("say the last number");
+        int LastNum = int.Parse(Console.ReadLine());
 
-int Par = 0;
-int impar = 0;
+        int Par = 0;
+        int impar = 0;
 
-Console.WriteLine("\nNumero impar");
-for (int i = FirstNum; i <= LastNum; i++)
-{
-    if (i % 2 != 0)
-    {
-        Console.WriteLine(i);
-        impar++;
+        Console.WriteLine("\nNumero impar");
+        for (int i = FirstNum; i <= LastNum; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+                impar++;
+            }
+        }
+        Console.WriteLine("\nNúmeros pares");
+        for (int i = FirstNum; i <= LastNum; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+                Par++;
+            }
+        }
+
+        Console.WriteLine($"Quantidade de números impares: {impar}");
+        Console.WriteLine($"Quantidades de números pares: {Par}");
+
     }
-}
-Console.WriteLine("\nNúmeros pares");
-for (int i = FirstNum; i <= LastNum; i++)
-{
-    if (i % 2 == 0)
-    {
-        Console.WriteLine(i);
-        Par++;
-    }
-}
-
-Console.WriteLine($"Quantidade de números impares: {impar}");
-Console.WriteLine($"Quantidades de números pares: {Par}");
-
 }
